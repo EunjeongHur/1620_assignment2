b7f6bff569eb41a8f1a1c2a7482a33effcbc7221
eunjeong <ungdunge2@gmail.com>
2021-11-20 15:35:23 -0800

    create cleanUpCreate function

--- script.js
+++ script.js
@@ -41,3 +41,8 @@ function renderView(singlecontact) {
 	let addsinglecontact = document.querySelector('.main')
 	addsinglecontact.innerHTML += `<div class="contactinfo"><div class="contactname">${singlecontact.name}<img src="./img/profile.jpg" class="profilepic" alt="Profile picture"></div><div class="contactemail">email: ${singlecontact.email}</div><div class="contactphone">cell: +1 ${singlecontact.phone}</div><div class="contactaddress">address: ${singlecontact.address}</div><div class="buttons"><button class="button edit" value="Edit">Edit</button><button class="button close" value="Close">Close</button></div></div>`;
 }
+
+function cleanUpCreate() {
+	const createcards = document.querySelector('.contactedit')
+	createcards.remove()
+}
