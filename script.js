7500c5ed6e65fb9f2679b9047982f19e42d4c3e8
eunjeong <ungdunge2@gmail.com>
2021-11-20 15:26:41 -0800

    create renderView function

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const contactList = [ 
 	{ 
 		name: "Oliver Queen", 
 		phone: "778-555-1234", 
-		address: "101 Main St, Star City, USA",    
+		address: "101 Main St, Star City, USA",  
 		email: "greenarrow@watchtower.com",  
 	},   
 	{    
@@ -33,6 +33,11 @@ function renderIndex(contact) {
 }
 
 function cleanUpView() {
-	const viewcards = document.querySelector('.contactinfo') //.contactemail .contactphone .contactaddress .buttons')
+	const viewcards = document.querySelector('.contactinfo')
 	viewcards.remove()
+}
+
+function renderView(singlecontact) {
+	let addsinglecontact = document.querySelector('.main')
+	addsinglecontact.innerHTML += `<div class="contactinfo"><div class="contactname">${singlecontact.name}<img src="./img/profile.jpg" class="profilepic" alt="Profile picture"></div><div class="contactemail">email: ${singlecontact.email}</div><div class="contactphone">cell: +1 ${singlecontact.phone}</div><div class="contactaddress">address: ${singlecontact.address}</div><div class="buttons"><button class="button edit" value="Edit">Edit</button><button class="button close" value="Close">Close</button></div></div>`;
 }
