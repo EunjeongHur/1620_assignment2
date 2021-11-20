0b5f9c0767c1688618dae3ffc6a76d95cdbff77c
eunjeong <ungdunge2@gmail.com>
2021-11-20 13:40:44 -0800

    create createsingleIndex function

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function cleanUpIndex() {
         elem.remove();
     }
 }
+
+const contactList = [  
+	{ 
+		name: "Oliver Queen", 
+		phone: "778-555-1234", 
+		address: "101 Main St, Star City, USA",    
+		email: "greenarrow@watchtower.com",  
+	},   
+	{    
+		name: "Jessica Cruz",    
+		phone: "123-555-5555",    
+		address: "Portland Oregon",    
+		email: "greenlantern@watchtower.com",  
+	}
+]
+
+function createSingleIndex() {
+    const singlecontact = '<div class="contact"><p>${newcontact}</p></div>';
+	return singlecontact
+}
