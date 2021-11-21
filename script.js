f4849f20fe5121a21d61180a7858356164f997b1
eunjeong <ungdunge2@gmail.com>
2021-11-20 16:15:02 -0800

    create renderCreate function

--- script.js
+++ script.js
@@ -38,11 +38,69 @@ function cleanUpView() {
 }
 
 function renderView(singlecontact) {
-	let addsinglecontact = document.querySelector('.main')
-	addsinglecontact.innerHTML += `<div class="contactinfo"><div class="contactname">${singlecontact.name}<img src="./img/profile.jpg" class="profilepic" alt="Profile picture"></div><div class="contactemail">email: ${singlecontact.email}</div><div class="contactphone">cell: +1 ${singlecontact.phone}</div><div class="contactaddress">address: ${singlecontact.address}</div><div class="buttons"><button class="button edit" value="Edit">Edit</button><button class="button close" value="Close">Close</button></div></div>`;
+	addsinglecontact = `
+	<div class="contactinfo">
+        <div class="contactname">
+            ${singlecontact.name}
+            <img src="./img/profile.jpg" class="profilepic" alt="Profile picture">
+        </div>
+        <div class="contactemail">email: ${singlecontact.email}
+		</div>
+        <div class="contactphone">cell: +1 ${singlecontact.phone}
+		</div>
+        <div class="contactaddress">address: ${singlecontact.address}
+		</div>
+        <div class="buttons">
+            <button class="button edit" value="Edit">Edit</button>
+            <button class="button close" value="Close">Close</button>
+        </div>
+    </div>
+	`
+	document.querySelector('.main').innerHTML = addsinglecontact
 }
+	
 
 function cleanUpCreate() {
 	const createcards = document.querySelector('.contactedit')
 	createcards.remove()
+}
+
+function renderCreate() {
+	addcontact = `
+	<div class="contactedit">
+		<div class="contactimg">
+			<img src="./img/profile.jpg" class ="profilepic" alt="Profile picture">
+			</div>
+		<div class="form">
+			<form>
+				<div class="inputcontainer">
+					<input type="text" id="contactname" name="contactname" placeholder="Contact Name">
+					<button class="extrafield" id="extranamefield" name="extranamefield">+</button>
+				</div>
+
+				<div class="inputcontainer">
+					<input type="tel" id="contactphone" name="contactphone" placeholder="Contact Phone">
+					<button class="extrafield" id="extraphonefield" name="extraphonefield">+</button>
+				</div>
+
+				<div class="inputcontainer">
+					<input type="text" id="contactaddress" name="contactaddress" placeholder="Contact Address">
+					<button class="extrafield" id="extraaddressfield" name="extraaddressfield">+</button>
+				</div>
+				
+				<div class="inputcontainer">
+					<input type="email" id="contactemail" name="contactemail" placeholder="Contact Email">
+					<button class="extrafield" id="extraemailfield" name="extraemailfield">+</button>
+				</div>
+
+				<div class="buttons">
+					<button type="submit" class="button save" id="savecontact" name="savecontact">Save Contact</button>
+					<button type="reset" class="button cancel" id="cancel" name="cancel">Cancel</button>
+				</div>
+			</form>
+		</div>
+	</div>
+	`;
+
+	document.querySelector('.main').innerHTML = addcontact
 }
